68f20af252520bc6ebd7efb99422c6f85f13217b
NatalliaFilipchyk <106901109+natty1978@users.noreply.github.com>
2023-10-09 18:48:05 +0300

    Update script DATA.sql

--- script DATA.sql
+++ script DATA.sql
@@ -1,24 +1,57 @@
-ALTER TABLE drivers
-ADD contract_nr CHAR(9);
+Cкрипты на редактирование схемы:
 
-ALTER TABLE drivers
-CHANGE contract_nr contract_data DATE;
+На создание БД
+CREATE DATABASE transport_company;
 
+создание таблицы
+CREATE TABLE if not exists drivers
+ (
+    driver_id INT, auto_increment INT primary key unique, 
+    full_name VARCHAR(120), 
+    license_nr VARCHAR(50), 
+    status enum ('domestic', 'international'),
+    phone VARCHAR(50),
+    FOREIGN KEY (truck_nr) REFERENCES trucks(truck_nr) on delete cascade );
+    
+ переименование таблицы 
+RENAME TABLE drivers TO staff;
+
+
+связь M-M (через доп. таблицу driver-truck - то есть в дополнение к первой drivers еще + 2 таблицы создаю: trucks 3) driver_truck)
+  CREATE TABLE if not exists trucks
+ (
+    truck_nr INT,  primary key
+    model_truck VARCHAR(50),
+    truck_type enum ('tent', 'frigo'),
+    insurance_nr VARCHAR(50), unique not null, 
+    year INT unique not null, 
+FOREIGN KEY (truck_nr) REFERENCES trucks(truck_nr) on delete cascade
+);
+
+CREATE TABLE driver_truck (
+    driver_id INT,
+    truck_nr CHAR(12),
+    PRIMARY KEY (driver_id, truck_nr),
+    FOREIGN KEY (driver_id) REFERENCES drivers(driver_id),
+    FOREIGN KEY (truck_nr) REFERENCES trucks(truck_nr)
+);
+
+добавление колонки в таблицу
+ALTER TABLE drivers
+ADD contract_nr INT;
+
+переименование колонки
+ALTER TABLE drivers
+CHANGE contract_nr contract_data INT;
+
+удаление колонки
 ALTER TABLE drivers
 DROP COLUMN contract_data;
 
-DROP TABLE if exists drivers
-CASCADE;
+удаление таблицы
+DROP TABLE drivers;
 
-INSERT INTO drivers (full_name)
-VALUES ('Mr_Ivan_Ivanych');
 
-DELETE FROM drivers
-WHERE full_name = 'Mr_Ivan_Ivanych';
 
-UPDATE drivers
-SET full_name = 'Mr_Ivan_Ivanych'
-WHERE full_name = 'Mr_Petr_Petrovich';
 
-SELECT * FROM drivers
-WHERE status = 'international' ;
+
